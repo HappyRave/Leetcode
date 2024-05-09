595671ef0780986efa116440778f356be3519845
HappyRave <badwolfinet@gmail.com>
2024-05-09 23:34:00 +0200

    chore: Refactor p234_is_palindrome_list.rs for readability and test coverage

--- src/solutions/p234_is_palindrome_list.rs
+++ src/solutions/p234_is_palindrome_list.rs
@@ -1,17 +1,4 @@
-use super::Solution;
-
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
+use super::{ListNode, Solution};
 
 impl Solution {
     pub fn is_palindrome_list(head: Option<Box<ListNode>>) -> bool {
@@ -36,26 +23,19 @@ impl Solution {
 
 #[cfg(test)]
 mod tests {
+    use crate::solutions::ListExt;
+
     use super::*;
 
     #[test]
     fn test_is_palindrome_list() {
-        let mut node1 = ListNode::new(1);
-        let mut node2 = ListNode::new(2);
-        let mut node3 = ListNode::new(2);
-        let node4 = ListNode::new(1);
-        node3.next = Some(Box::new(node4));
-        node2.next = Some(Box::new(node3));
-        node1.next = Some(Box::new(node2));
-        assert_eq!(Solution::is_palindrome_list(Some(Box::new(node1))), true);
-
-        let mut node1 = ListNode::new(1);
-        let mut node2 = ListNode::new(2);
-        let mut node3 = ListNode::new(3);
-        let node4 = ListNode::new(1);
-        node3.next = Some(Box::new(node4));
-        node2.next = Some(Box::new(node3));
-        node1.next = Some(Box::new(node2));
-        assert_eq!(Solution::is_palindrome_list(Some(Box::new(node1))), false);
+        assert_eq!(
+            Solution::is_palindrome_list(vec![1, 2, 2, 1].into_list()),
+            true
+        );
+        assert_eq!(
+            Solution::is_palindrome_list(vec![1, 2, 3, 1].into_list()),
+            false
+        );
     }
 }
