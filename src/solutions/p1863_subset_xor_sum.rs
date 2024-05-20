091c2143a0043872fd080c5271bee1a9b6ff4e45
HappyRave <badwolfinet@gmail.com>
2024-05-20 18:42:57 +0200

    feat: Add solution for p1863_subset_xor_sum problem

---
+++ src/solutions/p1863_subset_xor_sum.rs
@@ -0,0 +1,34 @@
+use super::Solution;
+
+impl Solution {
+    pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+        let mut res = 0;
+        for &num in &nums {
+            res |= num;
+        }
+        res << (nums.len() - 1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_subset_xor_sum() {
+        let nums = vec![1, 3];
+        assert_eq!(Solution::subset_xor_sum(nums), 6);
+    }
+
+    #[test]
+    fn test_subset_xor_sum_2() {
+        let nums = vec![5, 1, 6];
+        assert_eq!(Solution::subset_xor_sum(nums), 28);
+    }
+
+    #[test]
+    fn test_subset_xor_sum_3() {
+        let nums = vec![3, 4, 5, 6, 7, 8];
+        assert_eq!(Solution::subset_xor_sum(nums), 480);
+    }
+}
