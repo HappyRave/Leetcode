8bf5b644c9f3876e1a8772baadf08ef8a3cfda18
HappyRave <badwolfinet@gmail.com>
2024-06-02 18:23:27 +0200

    feat: Add solution for p344_reverse_string problem

---
+++ src/solutions/array/p344_reverse_string.rs
@@ -0,0 +1,32 @@
+use crate::solutions::Solution;
+
+impl Solution {
+    pub fn reverse_string(s: &mut [char]) {
+        let mut left = 0;
+        let mut right = s.len() - 1;
+        while left < right {
+            s.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reverse_string() {
+        let mut s = vec!['h', 'e', 'l', 'l', 'o'];
+        Solution::reverse_string(&mut s);
+        assert_eq!(s, vec!['o', 'l', 'l', 'e', 'h']);
+    }
+
+    #[test]
+    fn test_reverse_string_2() {
+        let mut s = vec!['H', 'a', 'n', 'n', 'a', 'h'];
+        Solution::reverse_string(&mut s);
+        assert_eq!(s, vec!['h', 'a', 'n', 'n', 'a', 'H']);
+    }
+}
