c517b2c9a3e23f80e99c96e2dd43be22089644c9
HappyRave <badwolfinet@gmail.com>
2024-06-08 10:58:29 +0200

    feat: Add solution for p523_check_subarray_sum problem

---
+++ src/solutions/array/p523_check_subarray_sum.rs
@@ -0,0 +1,49 @@
+use crate::solutions::Solution;
+
+impl Solution {
+    pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
+        let mut sum = 0;
+        let mut map: std::collections::HashMap<i32, i32> = std::collections::HashMap::new();
+        map.insert(0, -1);
+        for i in 0..nums.len() {
+            sum += nums[i];
+            if k != 0 {
+                sum %= k;
+            }
+            if let Some(&prev) = map.get(&sum) {
+                if i as i32 - prev > 1 {
+                    return true;
+                }
+            } else {
+                map.insert(sum, i as i32);
+            }
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_check_subarray_sum() {
+        let nums = vec![23, 2, 4, 6, 7];
+        let k = 6;
+        assert_eq!(Solution::check_subarray_sum(nums, k), true);
+    }
+
+    #[test]
+    fn test_check_subarray_sum_2() {
+        let nums = vec![23, 2, 6, 4, 7];
+        let k = 13;
+        assert_eq!(Solution::check_subarray_sum(nums, k), false);
+    }
+
+    #[test]
+    fn test_check_subarray_sum_3() {
+        let nums = vec![2, 4, 3];
+        let k = 6;
+        assert_eq!(Solution::check_subarray_sum(nums, k), true);
+    }
+}
