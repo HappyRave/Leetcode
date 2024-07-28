8c4544c27349ae8dcb7d554fdfefb9b85f049d2c
HappyRave <badwolfinet@gmail.com>
2024-07-28 11:47:36 +0200

    feat: Add solution for p2045_second_minimum problem

--- src/solutions/graph/mod.rs
+++ src/solutions/graph/mod.rs
@@ -1,5 +1,6 @@
 pub mod p1334_find_the_city;
 pub mod p1579_max_num_edges_to_remove;
 pub mod p1791_find_center;
+pub mod p2045_second_minimum;
 pub mod p2192_get_ancestors;
 pub mod p2285_maximum_importance;
