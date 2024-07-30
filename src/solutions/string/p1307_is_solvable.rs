ac7777a50c140abbde38088002af6ced1faa74fa
HappyRave <badwolfinet@gmail.com>
2024-07-30 10:07:50 +0200

    working is_solvable, but super slow

--- src/solutions/string/p1307_is_solvable.rs
+++ src/solutions/string/p1307_is_solvable.rs
@@ -2,7 +2,7 @@ use crate::solutions::Solution;
 
 impl Solution {
     pub fn is_solvable(words: Vec<String>, result: String) -> bool {
-        fn to_equation(words: &Vec<String>, result: &str) -> Vec<(usize, usize)> {
+        fn to_equation(words: &Vec<String>, result: &str) -> Vec<(char, usize, usize)> {
             let mut char_map: std::collections::HashMap<char, (usize, usize)> =
                 std::collections::HashMap::with_capacity(10);
             for word in words {
@@ -16,28 +16,49 @@ impl Solution {
             println!("{:?}", char_map);
             char_map
                 .iter()
-                .map(|(_, (min, max))| (*min, *max))
-                .filter(|(min, max)| min != max)
+                .map(|(c, (min, max))| (*c, *min, *max))
+                //.filter(|(min, max)| min != max)
                 .collect()
         }
 
-        fn is_valid_answer(equation: &[(usize, usize)], answer: &[usize]) -> bool {
-            let mut left_hand = 0;
-            let mut right_hand = 0;
-            equation.iter().zip(answer).for_each(|((min, max), value)| {
-                left_hand += min * value;
-                right_hand += max * value;
-            });
-            left_hand == right_hand
+        fn is_valid_answer(
+            equation: &[(char, usize, usize)],
+            answer: &[usize],
+            words: &Vec<String>,
+            result: &str,
+        ) -> bool {
+            fn word_to_num(
+                word: &str,
+                equation: &[(char, usize, usize)],
+                answer: &[usize],
+            ) -> usize {
+                let mut word = word.clone().to_string();
+                for i in 0..equation.len() {
+                    let (c, _, _) = equation[i];
+                    let num = answer[i];
+                    //println!("{:?} {:?}", c, num);
+                    word = word.replace(c, &num.to_string());
+                }
+                //println!("{:?}", word);
+                word.parse().unwrap()
+            }
+            let left_hand_side = words
+                .iter()
+                .map(|word| word_to_num(word, equation, answer))
+                .sum::<usize>();
+            let right_hand_side = word_to_num(result, equation, answer);
+            left_hand_side == right_hand_side
         }
 
         fn is_solvable_backtrack(
-            equation: &[(usize, usize)],
+            equation: &[(char, usize, usize)],
             answer: &mut Vec<usize>,
             used: &mut std::collections::HashSet<usize>,
+            words: &Vec<String>,
+            result: &str,
         ) -> bool {
             if answer.len() == equation.len() {
-                return is_valid_answer(equation, answer);
+                return is_valid_answer(equation, answer, words, result);
             }
             for i in 0..10 {
                 if used.contains(&i) {
@@ -45,7 +66,7 @@ impl Solution {
                 }
                 answer.push(i);
                 used.insert(i);
-                if is_solvable_backtrack(equation, answer, used) {
+                if is_solvable_backtrack(equation, answer, used, words, result) {
                     return true;
                 }
                 answer.pop();
@@ -58,7 +79,7 @@ impl Solution {
         println!("{:?}", equation);
         let mut used: std::collections::HashSet<usize> = std::collections::HashSet::new();
         let mut answer = vec![];
-        let res = is_solvable_backtrack(&equation, &mut answer, &mut used);
+        let res = is_solvable_backtrack(&equation, &mut answer, &mut used, &words, &result);
         println!("{:?}", answer);
         res
     }
