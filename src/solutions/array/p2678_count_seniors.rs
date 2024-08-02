057ab795c53b24259b076527f2f0b96cf846c085
HappyRave <badwolfinet@gmail.com>
2024-08-02 21:46:35 +0200

    Refactor test_count_seniors_1 to test_count_seniors_2 and add test_count_seniors_3

--- src/solutions/array/p2678_count_seniors.rs
+++ src/solutions/array/p2678_count_seniors.rs
@@ -23,8 +23,14 @@ mod tests {
     }
 
     #[test]
-    fn test_count_seniors_1() {
+    fn test_count_seniors_2() {
         let details = "[\"1313579440F2036\",\"2921522980M5644\"]".to_string_vec();
         assert_eq!(Solution::count_seniors(details), 0);
+    }
+
+    #[test]
+    fn test_count_seniors_3() {
+        let details = "[\"9751302862F0693\",\"3888560693F7262\",\"5485983835F0649\",\"2580974299F6042\",\"9976672161M6561\",\"0234451011F8013\",\"4294552179O6482\"]".to_string_vec();
+        assert_eq!(Solution::count_seniors(details), 4);
     }
 }
