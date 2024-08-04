e5db6d36725324eed1a4c60070e52f0a3b6e2f0b
HappyRave <badwolfinet@gmail.com>
2024-08-04 22:27:39 +0200

    rename tests for validate_stack_sequence

--- src/solutions/stack/p946_validate_stack_sequences.rs
+++ src/solutions/stack/p946_validate_stack_sequences.rs
@@ -20,14 +20,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_1() {
+    fn test_validate_stack_sequences() {
         let pushed = vec![1, 2, 3, 4, 5];
         let popped = vec![4, 5, 3, 2, 1];
         assert!(Solution::validate_stack_sequences(pushed, popped));
     }
 
     #[test]
-    fn test_2() {
+    fn test_validate_stack_sequences_2() {
         let pushed = vec![1, 2, 3, 4, 5];
         let popped = vec![4, 3, 5, 1, 2];
         assert!(!Solution::validate_stack_sequences(pushed, popped));
