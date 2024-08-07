e8fd6b74bd5c26575aa9ea984b18e8f94d6190a7
HappyRave <badwolfinet@gmail.com>
2024-08-08 00:52:56 +0200

    feat: Add solution for p2577_minimum_time problem - NOT WORKING

--- src/solutions/matrix/mod.rs
+++ src/solutions/matrix/mod.rs
@@ -3,6 +3,7 @@ pub mod p1536_min_swaps;
 pub mod p1605_restore_matrix;
 pub mod p2373_largest_local;
 pub mod p2392_build_matrix;
+pub mod p2577_minimum_time;
 pub mod p2812_maximum_safeness_factor;
 pub mod p835_largest_overlap;
 pub mod p861_matrix_score;
